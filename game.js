9df599158b46506cd1a13bef125e0877bfbeba58
JaydonFernandes <60637128+JaydonFernandes@users.noreply.github.com>
2020-02-15 16:46:55 -0700

    Add files via upload

---
+++ game.js
@@ -0,0 +1,21 @@
+const game = new Phaser.game(800, 600, Phaser.AUTO, '', {
+    preload: preload,
+    create: create,
+    update: update
+})
+
+function preload(){}
+function create(){}
+function update(){}
+
+/*
+
+Project Init - Jaydon
+Project Init - Victor 
+Research - Justin
+Research 
+Design - David
+
+
+
+*/
