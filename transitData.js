899cedea4c70e88ccdab980b6a87617a54398405
JaydonFernandes <37673897+JaydonFernandes@users.noreply.github.com>
2020-02-15 21:45:01 -0700

    added

---
+++ transitData.js
@@ -0,0 +1,285 @@
+var trainStations = {
+    saddleTowne: { 
+        xCord: 1500,
+        yCord: 100
+    },
+    martindale: {
+        xCord: 1500,
+        yCord: 200
+    },
+    westwinds: {
+        xCord: 1500,
+        yCord: 300
+    },
+    whitehorn: {
+        xCord: 1500,
+        yCord: 400
+    },
+    rundle : {
+        xCord: 1500,
+        yCord: 500
+    },
+    marlborough :  {
+        xCord: 1500,
+        yCord: 600
+    },
+    franklin :  {
+        xCord: 1500,
+        yCord: 100
+    },
+    maxBell :  {
+        xCord: 1500,
+        yCord: 100
+    },
+    zoo :  {
+        xCord: 1500,
+        yCord: 100
+    },
+    bridgeLand :  {
+        xCord: 1500,
+        yCord: 100
+    },
+    cityHall :  {
+        xCord: 1500,
+        yCord: 100
+    },
+    firstStreet :  {
+        xCord: 1500,
+        yCord: 100
+    },
+    fourthStreet : {
+        xCord: 1500,
+        yCord: 100
+    },
+    seventhStreet : {
+        xCord: 1500,
+        yCord: 100
+    },
+    downtownWest : {
+        xCord: 1500,
+        yCord: 100
+    },
+    sunalta : {
+        xCord: 1500,
+        yCord: 100
+    },
+    shaganappi : {
+        xCord: 1500,
+        yCord: 100
+    },
+    westbrook : {
+        xCord: 1500,
+        yCord: 100
+    },
+    fourtyFifthStreet : {
+        xCord: 1500,
+        yCord: 100
+    },
+   sirocco : {
+        xCord: 1500,
+        yCord: 100
+    },
+    sixtyNinthStreet : {
+        xCord: 1500,
+        yCord: 100
+    },
+    eigthStreet : {
+        xCord: 1500,
+        yCord: 100
+    },
+    sixthStreet : {
+        yCord: 100
+    },
+    thirdStreet : {
+        xCord: 1500,
+        yCord: 100
+    },
+    centreStreet : {
+        xCord: 1500,
+        yCord: 100
+    },
+    tuscany : {
+        xCord: 1500,
+        yCord: 100
+    },
+    crowfoot : {
+        xCord: 1500,
+        yCord: 100
+    },
+    dalhousie : {
+        xCord: 1500,
+        yCord: 100
+    },
+    brentwood : {
+        xCord: 1500,
+        yCord: 100
+    },
+    university : {
+        xCord: 1500,
+        yCord: 100
+    },
+    banffTrail : {
+        xCord: 1500,
+        yCord: 100
+    },
+    lionsPark : {
+        xCord: 1500,
+        yCord: 100
+    },
+    sait : {
+        xCord: 1500,
+        yCord: 100
+    },
+    sunnyside : {
+        xCord: 1500,
+        yCord: 100
+    },
+    victoriaPark : {
+        xCord: 1500,
+        yCord: 100
+    },
+    erlton : {
+        xCord: 1500,
+        yCord: 100
+    },
+    thirtyNinth : {
+        xCord: 1500,
+        yCord: 100
+    },
+    chinook: {
+        xCord: 1500,
+        yCord: 100
+    },
+    heritage : {
+        xCord: 1500,
+        yCord: 100
+    },
+    southland : {
+        xCord: 1500,
+        yCord: 100
+    },
+    anderson : {
+        xCord: 1500,
+        yCord: 100
+    },
+    canyonMeadows : {
+        xCord: 1500,
+        yCord: 100
+    },
+    fishCreek : {
+        xCord: 1500,
+        yCord: 100
+    },
+    shawnessy : {
+        xCord: 1500,
+        yCord: 100
+    },
+    somerset : {
+        xCord: 1500,
+        yCord: 100
+    }
+}
+
+var blueLine69St = [
+    trainStations.saddleTowne,
+    trainStations.martindale,
+    trainStations.westwinds,
+    trainStations.whitehorn,
+    trainStations.rundle,
+    trainStations.marlborough,
+    trainStations.franklin,
+    trainStations.maxBell,
+    trainStations.zoo,
+    trainStations.bridgeLand,
+    trainStations.cityHall,
+    trainStations.firstStreet,
+    trainStations.fourthStreet,
+    trainStations.seventhStreet,
+    trainStations.downtownWest,
+    trainStations.sunalta,
+    trainStations.shaganappi,
+    trainStations.westbrook,
+    trainStations.fourtyFifthStreet,
+    trainStations.sirocco,
+    trainStations.sixtyNinthStreet
+] 
+
+var blueLineSaddletowne = [
+    trainStations.sixtyNinthStreet,
+    trainStations.sirocco,
+    trainStations.fourtyFifthStreet,
+    trainStations.westbrook,
+    trainStations.shaganappi,
+    trainStations.sunalta,
+    trainStations.downtownWest,
+    trainStations.eighthStreet,
+    trainStations.sixthStreet,
+    trainStations.thirdStreet,
+    trainStations.centerStreet,
+    trainStations.cityHall,
+    trainStations.bridgeLand,
+    trainStations.zoo,
+    trainStations.maxBell,
+    trainStations.franklin,
+    trainStations.marlborough,
+    trainStations.rundle,
+    trainStations.whitehorn,
+    trainStations.westwinds,
+    trainStations.martindale,
+    trainStations.saddleTown        
+]
+
+var redLineTuscany = [
+    trainStations.somerset,
+    trainStations.shawnessy,
+    trainStations.fishCreek,
+    trainStations.canyonMeadows,
+    trainStations.anderson,
+    trainStations.southland,
+    trainStations.heritage,
+    trainStations.chinook,
+    trainStations.thirtyNinth,
+    trainStations.erlton,
+    trainStations.victoriaPark,
+    trainStations.cityHall,
+    trainStations.firstStreet,
+    trainStations.fourthStreet,
+    trainStations.seventhStreet,
+    trainStations.sunnyside,
+    trainStations.sait,
+    trainStations.lionsPark,
+    trainStations.banffTrail,
+    trainStations.university,
+    trainStations.brentwood,
+    trainStations.dalhousie,
+    trainStations.crowfoot,
+    trainStations.tuscany
+]
+
+var redLineSomerset = [
+    trainStations.tuscany,
+    trainStations.crowfoot,
+    trainStations.dalhousie,
+    trainStations.brentwood,
+    trainStations.university,
+    trainStations.banffTrail,
+    trainStations.lionsPark,
+    trainStations.sait,
+    trainStations.sunnyside,
+    trainStations.eigthStreet,
+    trainStations.sixthStreet,
+    trainStations.thirdStreet,
+    trainStations.centreStreet,
+    trainStations.cityHall,
+    trainStations.victoriaPark,
+    trainStations.erlton,
+    trainStations.thirtyNinth,
+    trainStations.chinook,
+    trainStations.heritage,
+    trainStations.southland,
+    trainStations.anderson,
+    trainStations.canyonMeadows,
+    trainStations.fishCreek,
+    trainStations.shawnessy,
+    trainStations.somerset
+]
